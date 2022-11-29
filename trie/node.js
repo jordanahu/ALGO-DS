f1ee7dba96412192efc09fce02e4a8bf4dbfe3aa
JAM <ahujordan@yahoo.com>
2022-11-29 07:01:01 +0100

    changed array implementation to hash table

--- trie/node.js
+++ trie/node.js
@@ -1,5 +1,5 @@
 module.exports = class{
-    constructor(value=" ", isEnd=false, children=[]){
+    constructor(value=" ", isEnd=false, children={}){
         this.value = value;
         this.isEnd = isEnd;
         this.children = children
